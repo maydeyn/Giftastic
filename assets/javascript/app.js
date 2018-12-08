b4b4424ef40e13a6bc6b294d07b066df1cc8944c
May Z <maydeyn@gmail.com>
2018-12-08 03:32:07 -0600

    updated code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,84 @@
 $(document).ready(function() {
+  // Variables
+  var movies = [
+    "Bohemian Rhapsody",
+    "Venom",
+    "Crazy Rich Asians",
+    "A Star is Born",
+    "Deadpool",
+    "Black Panther",
+    "Ready Player One"
+  ];
+  //
+  function displayMovieInfo() {
+    var movie = $(this).attr("data-name");
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      movie +
+      "&limit=10&api_key=YzfDBOYxh1fD4LYdSEJB4lrJvZBqzOfU";
 
-    // Variables
-    var movies = ["Bohemian Rhapsody", "Venom", "Crazy Rich Asians", "A Star is Born", "Deadpool", "Black Panther", "Ready Player One"];
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#movies-view").text(JSON.stringify(response));
+      console.log(response);
 
+      var gifResponse = response.data;
 
-    // Function
-    function displayGifButtons(){
-        $("#gifButtonsView").empty(); 
-        for (var i = 0; i < movies.length; i++){
-            var gifButton = $("<button>");
-            gifButton.addClass("movie");
-            gifButton.addClass("btn btn-primary")
-            gifButton.attr("data-name", movies[i]);
-            gifButton.text(movies[i]);
-            $("#gifButtonsView").append(gifButton);
-        }
-    }
-    // Function to add a new movie button
-        $("#addGif").on("click", function(event){
-            event.preventDefault();
+      for (var i = 0; i < gifResponse.length; i++) {
+        var gifPreview = $("<img>");
+        gifPreview.addClass("gif");
+        gifPreview.attr("src", results[i].images.fixed_height_still.url);
+        gifPreview.attr("data-still", results[i].images.fixed_height_still.url);
+        gifPreview.attr("data-animate", results[i].images.fixed_height.url);
+        gifPreview.attr("data-state", "still");
 
-        var movie = $("#movie-input").val().trim();
-        if (movie == ""){
-          return false;
-        }
-        movies.push(movie);
-    
-        displayGifButtons();
+        var newItemdiv = $('<div class="newItem">');
+        var gifRated = response.data[i].Rated;
+        var divRating = $("<p>").text("Rating: " + gifRated);
 
+        newItemdiv.append(divRating);
+        newItemdiv.append(dataImage);
+      }
     });
+    displayMovieInfo();
+  }
+  // create buttons
+  function renderButtons() {
+    // deleting previous gifs when new button is clicked
+    $("#gifButtonsView").empty();
 
-    function displayGifs(){
-        var movie = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
-        console.log(queryURL); 
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response); 
-            $("#gifsView").empty(); 
-            var results = response.data; 
-            if (results === ""){
-              alert("We couldn't find any Gifs! :(");
-            }
-            for (var i=0; i<results.length; i++){
-    
-                var gifDiv = $("<div>"); 
-                // gifDiv.addClass("gifDiv");
-
-                // rating of gif
-                var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                gifDiv.prepend(gifRating);
-                // pulling gif
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_small_still.url); 
-                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); 
-                gifImage.attr("data-animate",results[i].images.fixed_height_small.url); 
-
-                //image state
-                gifImage.attr("data-state", "still"); 
-                gifImage.addClass("image");
-                gifDiv.prepend(gifImage);
-                // pulling still image of gif
-                // adding div of gifs to gifsView div
-                $("#gifsView").prepend(gifDiv);
-            }
-        });
+    for (var i = 0; i < movies.length; i++) {
+      var newBtn = $("<button>");
+      newBtn.addClass("movie-btn");
+      newBtn.addClass("btn btn-primary");
+      newBtn.attr("data-name", movies[i]);
+      newBtn.text(movies[i]);
+      $("#gifButtonsView").append(newBtn);
     }
-    // Calling Functions & Methods
-    displayGifButtons(); 
-    // addNewButton();
+  }
 
+  $("#add-movie").on("click", function(event) {
+    event.preventDefault();
+    var movie = $("#movie-input")
+      .val()
+      .trim();
+    movies.push(movie);
+  });
 
-    // pausing Gifs
-    $(document).on("click", ".movie", displayGifs);
-    $(document).on("click", ".image", function(){
-        var state = $(this).data('state');
-        if ( state === 'still'){
-            $(this).attr('src', $(this).data('animate'));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
-    
-});
+  renderButtons();
+
+  $("#gifsView").on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+  $(document).on("click", ".movie-btn", displayMovieInfo);
+  renderButtons();
+}); //document ready closing
